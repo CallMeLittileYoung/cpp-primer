9668263eb03ebd5c5c53faaf32b4209a39579e26
liyuedong <liyuedong@joyy.com>
2021-11-02 14:31:59 +0800

    提交

--- src/chapter2/variable.cpp
+++ src/chapter2/variable.cpp
@@ -4,8 +4,27 @@
 /**
  * 有无符号类型变量时，结果也是无符号的aa bb cc dd
  */
-int main(void) {
+void decl();
 
+int used = 42 ; //全局作用域
+
+int main(void) {
+	decl();
+	define();
+	int unique = 10; //块作用域 局部变量
+
+	std::cout << used << " " << unique << std::endl;
+}
+
+void define(){
+ extern int i = 10; //定义
+ std::cout << i <<std::endl;
+ int y ; //定义
+ std::cout << y <<std::endl;
+ extern int z ;
+ std::cout << z <<std::endl;
+}
+void decl(){
 	unsigned u = 10, u2= 42; // 无符号数
 	std::cout << u2 -u << std::endl; // 正确 32
 	std::cout << u -u2 << std::endl; //类型为无符号数 故 -32 + int.max
